6cfb06cd32f6ac4ebe4908fb2985ad3488df6175
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2016-03-14 22:07:08 +0100

    Added routing

--- Samples.Front/src/ts/heroes/hero-detail.component.ts
+++ Samples.Front/src/ts/heroes/hero-detail.component.ts
@@ -1,14 +1,33 @@
-import {Component} from 'angular2/core';
-import {Hero} from './hero';
+import { Component, OnInit } from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
     moduleId: module.id,
     templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css'],
     inputs: ['hero']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     hero: Hero;
+
+    constructor(
+        private _heroService: HeroService,
+        private _routeParams: RouteParams) {
+    }
+
+    ngOnInit() {
+        let id = +this._routeParams.get('id');
+        this._heroService.getHero(id)
+            .then(hero => this.hero = hero);
+    }
+
+    goBack() {
+        window.history.back();
+    }
 }
 
 
